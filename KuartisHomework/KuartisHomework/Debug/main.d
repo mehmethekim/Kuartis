0d9c03300566bcc1ccde2a4bc3ec1ca79eae16f8
Mehmet Hekimoğlu <mehmethekimolu@gmail.com>
2021-10-05 19:29:07 +0300

    Tried Opening and closing LEDs

---
+++ KuartisHomework/KuartisHomework/Debug/main.d
@@ -0,0 +1,49 @@
+main.d main.o: .././main.c \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Users\mehme\Desktop\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom808.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\xmega.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ .././PortDefinitions.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Users\mehme\Desktop\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom808.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\xmega.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+.././PortDefinitions.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\users\mehme\desktop\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- KuartisHomework/KuartisHomework/main.c
+++ KuartisHomework/KuartisHomework/main.c
@@ -2,17 +2,35 @@
  * KuartisHomework.c
  *
  * Created: 05/10/2021 15:59:25
- * Author : mehme
+ * Author : Mehmet Hekimoðlu
  */ 
 
 #include <avr/io.h>
-
+#include "PortDefinitions.h"
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
+	// LED TEST RUN
+	PORTA.DIR = (1<<LED_1) | (1<<LED_2) | (1<<LED_3) | (1<<LED_4) ;
+	PORTA.OUT = 0b11111111;
+	PORTA.OUT = 0b11011000; // Set all register to 1. 
+	
+	
+	//PORTA.OUT &= ~(1<<LED_1) & ~(1<<LED_2) & ~(1<<LED_3) & ~(1<<LED_4); // light all  -> 0b11011000
+	//PORTA.OUT |= (1<<LED_1)  | (1<<LED_3); // close 1 and 3 0b00000001 OR 0b00000010 0b00000011
+	PORTA.OUT &= ~(1<<LED_1) & ~(1<<LED_3);  // light all  1 3 -> 0b11111011 AND 0b11011111->0b11011011    
+	
+	
+	
+	
     while (1) 
     {
+		
+		
     }
 }
 
